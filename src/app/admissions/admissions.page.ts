8b24ce4537fef95d5fcb623670368118e12b2396
Mushaqdeen-J <mushaqdeen@yahoo.co.in>
2021-03-22 13:08:03 +0530

    Admissions section added

--- src/app/admissions/admissions.page.ts
+++ src/app/admissions/admissions.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admissions.page.scss'],
 })
 export class AdmissionsPage implements OnInit {
-
+  segment:string ='admissions';
   constructor() { }
 
   ngOnInit() {
